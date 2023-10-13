09f95a5719dd1d6a00a862fcec05407e376ea529
jorgemagic <jferrero@plainconcepts.com>
2023-10-13 17:38:36 +0200

    - Added floor colors

--- EverSneaks/Services/ControllerService.cs
+++ EverSneaks/Services/ControllerService.cs
@@ -10,11 +10,17 @@ namespace EverSneaks.Services
     public class ControllerService : Service
     {
         private MaterialComponent materialComponent;
+        private MaterialComponent floorMaterialComponent;
         private CameraBehavior cameraBehavior;
-        private Material grayMaterial;
-        private Material redMaterial;
-        private Material orangeMaterial;
-        private Material blueMaterial;
+        private Material graySneakersMaterial;
+        private Material redSneakersMaterial;
+        private Material orangeSneakersMaterial;
+        private Material blueSneakersMaterial;
+
+        private Material grayFloorMaterial;
+        private Material blueFloorMaterial;
+        private Material redFloorMaterial;
+        private Material orangeFloorMaterial;
 
         public enum SneakerColor
         {
@@ -43,10 +49,15 @@ namespace EverSneaks.Services
 
             // Load materials
             var assetsService = Application.Current.Container.Resolve<AssetsService>();
-            this.grayMaterial = assetsService.Load<Material>(EvergineContent.Models.AirJordan_Embedded.Materials.GrayMat);
-            this.redMaterial = assetsService.Load<Material>(EvergineContent.Models.AirJordan_Embedded.Materials.RedMat);
-            this.orangeMaterial = assetsService.Load<Material>(EvergineContent.Models.AirJordan_Embedded.Materials.OrangeMat);
-            this.blueMaterial = assetsService.Load<Material>(EvergineContent.Models.AirJordan_Embedded.Materials.BlueMat);
+            this.graySneakersMaterial = assetsService.Load<Material>(EvergineContent.Models.AirJordan_Embedded.Materials.GrayMat);
+            this.redSneakersMaterial = assetsService.Load<Material>(EvergineContent.Models.AirJordan_Embedded.Materials.RedMat);
+            this.orangeSneakersMaterial = assetsService.Load<Material>(EvergineContent.Models.AirJordan_Embedded.Materials.OrangeMat);
+            this.blueSneakersMaterial = assetsService.Load<Material>(EvergineContent.Models.AirJordan_Embedded.Materials.BlueMat);
+
+            this.grayFloorMaterial = assetsService.Load<Material>(EvergineContent.Materials.grayFloor);
+            this.blueFloorMaterial = assetsService.Load<Material>(EvergineContent.Materials.blueFloor);
+            this.redFloorMaterial = assetsService.Load<Material>(EvergineContent.Materials.redFloor);
+            this.orangeFloorMaterial = assetsService.Load<Material>(EvergineContent.Materials.orangeFloor);
 
             //  Select material component
             var screenContextManager = Application.Current.Container.Resolve<ScreenContextManager>();
@@ -54,6 +65,8 @@ namespace EverSneaks.Services
             {
                 var entity = scene.Managers.EntityManager.FindAllByTag("SneakersMesh").First();
                 this.materialComponent = entity.FindComponent<MaterialComponent>();
+                var floor = scene.Managers.EntityManager.FindAllByTag("Floor").First();
+                this.floorMaterialComponent = floor.FindComponent<MaterialComponent>();
                 this.cameraBehavior = scene.Managers.EntityManager.FindComponentsOfType<CameraBehavior>().First();
             };
         }
@@ -63,16 +76,20 @@ namespace EverSneaks.Services
             switch (this.sneakerColor)
             {
                 case SneakerColor.Gray:
-                    this.materialComponent.Material = this.grayMaterial;
+                    this.materialComponent.Material = this.graySneakersMaterial;
+                    this.floorMaterialComponent.Material = this.grayFloorMaterial;
                     break;
                 case SneakerColor.Red:
-                    this.materialComponent.Material = this.redMaterial;
+                    this.materialComponent.Material = this.redSneakersMaterial;
+                    this.floorMaterialComponent.Material = this.redFloorMaterial;
                     break;
                 case SneakerColor.Orange:
-                    this.materialComponent.Material = this.orangeMaterial;
+                    this.materialComponent.Material = this.orangeSneakersMaterial;
+                    this.floorMaterialComponent.Material = this.orangeFloorMaterial;
                     break;
                 case SneakerColor.Blue:
-                    this.materialComponent.Material = this.blueMaterial;
+                    this.materialComponent.Material = this.blueSneakersMaterial;
+                    this.floorMaterialComponent.Material = this.blueFloorMaterial;
                     break;
             }
         }
